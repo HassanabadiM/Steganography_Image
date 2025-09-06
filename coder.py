49bdf511c88f54bd83d154261385e18a9cb3481b
Mohammad H <160239689+HassanabadiM@users.noreply.github.com>
2025-09-06 10:05:12 +0330

    Update coder.py

--- coder.py
+++ coder.py
@@ -1,9 +1,9 @@
 from PIL import Image
 
-pass='@testtest'
+password='@testtest'
 img=Image.open('img.jpg')
 
-for i , chr in enumerate(pass):
+for i , chr in enumerate(password):
     r,g,b=img.getpixel((i*100,100))
     img.putpixel((i*100,100),(r,(ord(chr)+100),b))
 
