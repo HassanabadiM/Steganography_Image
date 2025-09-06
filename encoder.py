82d95fe22300c7a33e17faa340c9ca624f1f73c9
Mohammad H <160239689+HassanabadiM@users.noreply.github.com>
2025-09-06 10:13:43 +0330

    Update and rename coder.py to encoder.py

--- encoder.py
+++ encoder.py
@@ -1,11 +1,11 @@
 from PIL import Image
 
 password='testtest'
-img=Image.open('img.jpg')
+img=Image.open('image.jpg')
 
 for i , chr in enumerate(password):
     r,g,b=img.getpixel((i*100,100))
     img.putpixel((i*100,100),(r,(ord(chr)+100),b))
 
 
-img.save('img2.png')
+img.save('encoded_image.png')
