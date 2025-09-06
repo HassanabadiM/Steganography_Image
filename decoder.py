a0132f3aec6c77e86ff2b7a505ca9c8cbbc4b677
Mohammad H <160239689+HassanabadiM@users.noreply.github.com>
2025-09-06 10:14:20 +0330

    Update decoder.py

--- decoder.py
+++ decoder.py
@@ -3,7 +3,7 @@ from PIL import Image
 password=''
 len_considered_password=8
 
-img=Image.open('img2.png')
+img=Image.open('encoded_image.png')
 
 for i in range(len_considered_password):
     pix= img.getpixel((i*100,100))
