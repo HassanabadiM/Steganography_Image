efe1b13d5f58ed849177f9224f5b111fa8c33bc2
Mohammad H <160239689+HassanabadiM@users.noreply.github.com>
2025-09-06 10:10:28 +0330

    Create decoder.py

---
+++ decoder.py
@@ -0,0 +1,13 @@
+from PIL import Image
+
+password=''
+len_considered_password=8
+
+img=Image.open('img2.png')
+
+for i in range(len_considered_password):
+    pix= img.getpixel((i*100,100))
+    password+=chr(pix[1]-100)
+
+
+print(password)
